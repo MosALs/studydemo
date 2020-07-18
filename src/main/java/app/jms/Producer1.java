0ed186e766a0fbb45c34c4349840fe395474234f
Mostafa Alsabagh <mostafa.alsabagh@datagearbi.com>
2020-07-18 02:16:49 +0200

    Add clarifying comments.

--- src/main/java/app/jms/Producer1.java
+++ src/main/java/app/jms/Producer1.java
@@ -9,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Producer1 {
 
+    /*
+     * URL of the JMS server. DEFAULT_BROKER_URL will just mean that JMS server is on localhost
+     *
+     * default broker URL is : tcp://localhost:61616"
+     */
+//    private static String url = ActiveMQ.DEFAULT_BROKER_URL;
+
     @RequestMapping(name = "/produce/{message}" , method = RequestMethod.GET)
     public String produceMessage(@PathVariable String message){
-
 
 
 
